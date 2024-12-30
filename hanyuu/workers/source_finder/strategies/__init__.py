6a629ab4d65a8aab17cf73a55601af0d68d4d21a
mozhaa <mozhay2005@gmail.com>
2024-12-30 21:30:18 +0300

    remove previous source_finding

--- hanyuu/workers/source_finder/strategies/__init__.py
+++
@@ -1,5 +0,0 @@
-from .strategy import SourceFindingStrategy
-from .youtube import YoutubeFindingStrategy
-
-# strategies in priority order (from most prioritized to least)
-strategies = [YoutubeFindingStrategy("youtube_strategy")]
