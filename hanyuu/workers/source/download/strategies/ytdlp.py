8a2e4eda274e0b4e6333260674ccfefd050032b4
mozhaa <mozhay2005@gmail.com>
2025-01-08 20:25:59 +0300

    ytdlp simple error handling (fixed number of attempts)

--- hanyuu/workers/source/download/strategies/ytdlp.py
+++ hanyuu/workers/source/download/strategies/ytdlp.py
@@ -13,6 +13,10 @@ logger = logging.getLogger(__name__)
 
 
 class YtDlpStrategy(SourceDownloadStrategy):
+    def __init__(self, name: str, n_attempts: int = 2) -> None:
+        super().__init__(name)
+        self.n_attempts = n_attempts
+
     async def check(
         self, qitem_source_id: int, download_dir: str
     ) -> Tuple[bool, Optional[Callable[[], Awaitable[None]]]]:
@@ -31,8 +35,17 @@ class YtDlpStrategy(SourceDownloadStrategy):
                 "bv*[height>720][height<=1080]+ba/b[height>720][height<=1080]/bv*+ba/b",
             }
 
-            with yt_dlp.YoutubeDL(params=params) as ydl:
-                ret_code = ydl.download(qitem_source.path)
+            attempts = 1
+            ret_code = None
+            while attempts <= self.n_attempts:
+                logger.info(f"Attempt #{attempts}: trying to download {qitem_source.path}")
+                try:
+                    with yt_dlp.YoutubeDL(params=params) as ydl:
+                        ret_code = ydl.download(qitem_source.path)
+                    break
+                except yt_dlp.utils.DownloadError:
+                    attempts += 1
+                    logger.info("Download error occured")
 
             if ret_code == 0:
                 local_fp = str(next(Path(download_dir).glob(f"{qitem_source_id}.*")).resolve())
@@ -42,6 +55,9 @@ class YtDlpStrategy(SourceDownloadStrategy):
                     qitem_source.local_fp = local_fp
                     await session.commit()
             else:
-                logger.warning(f"Download failed, return code = {ret_code}")
+                if ret_code is None:
+                    logger.warning(f"Download failed after {self.n_attempts} attempts")
+                else:
+                    logger.warning(f"Download failed with return code {ret_code}")
 
         return True, run
