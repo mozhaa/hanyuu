b0f2db956ebcef22a54a93fd0c9ad22f329c016f
mozhaa <mozhay2005@gmail.com>
2025-01-10 20:53:58 +0300

    better quizpart worker (many cli arguments)

--- hanyuu/workers/quizpart/__main__.py
+++ hanyuu/workers/quizpart/__main__.py
@@ -7,17 +7,17 @@ from sqlalchemy import case, delete, label, literal_column, select
 
 from hanyuu.config import getenv
 from hanyuu.database.main.connection import get_engine
-from hanyuu.database.main.models import (
-    QItemDifficulty,
-    QItemSource,
-    QItemSourceTiming,
-    QuizPart,
-)
+from hanyuu.database.main.models import QItem, QItemDifficulty, QItemSource, QItemSourceTiming, QuizPart, Category
 from hanyuu.video.videomakers import VideoMakerBase, styles
-from hanyuu.workers.utils import restrict_callrate, worker_log_config
+from hanyuu.workers.difficulty.strategies import strategies as _d_strategies
+from hanyuu.workers.source.find.strategies import strategies as _s_strategies
+from hanyuu.workers.timing.strategies import strategies as _t_strategies
+from hanyuu.workers.utils import worker_log_config
 
-d_strategies = ["manual", "strategy_random"]
-t_strategies = ["manual", "strategy_default"]
+d_strategies = ["manual"] + [s.name for s in _d_strategies]
+t_strategies = ["manual"] + [s.name for s in _t_strategies]
+s_strategies = ["manual"] + [s.name for s in _s_strategies]
+
 root_dir = Path(getenv("resources_dir")) / "videos" / "quizparts"
 worker_dir = Path(getenv("resources_dir")) / "workers" / "quizpart"
 logger = logging.getLogger(__name__)
@@ -26,14 +26,14 @@ logger = logging.getLogger(__name__)
 async def run_videomaker(timing_id: int, difficulty_id: int, videomaker: VideoMakerBase) -> None:
     engine = await get_engine()
     async with engine.async_session() as session:
-        quiz_part = QuizPart(timing_id=timing_id, difficulty_id=difficulty_id, style="classic", local_fp="")
+        quiz_part = QuizPart(timing_id=timing_id, difficulty_id=difficulty_id, style=videomaker.name, local_fp="")
         session.add(quiz_part)
         await session.flush()
         await session.refresh(quiz_part)
-        output_fp = root_dir / f"{quiz_part.id}.mkv"
-        quiz_part.local_fp = str(output_fp.resolve())
+        output_fp = str(root_dir / f"{quiz_part.id}.mkv")
+        quiz_part.local_fp = output_fp
         try:
-            await videomaker.create_video(timing_id, difficulty_id, str(output_fp.resolve()))
+            await videomaker.create_video(timing_id, difficulty_id, output_fp)
             logger.info(f"Created quiz part on {output_fp}")
         except Exception as e:
             logger.warning("Video making failed!")
@@ -43,7 +43,7 @@ async def run_videomaker(timing_id: int, difficulty_id: int, videomaker: VideoMa
             await session.commit()
 
 
-async def run_job(k: bool, videomaker: VideoMakerBase) -> None:
+async def run_jobs(args: argparse.Namespace) -> None:
     engine = await get_engine()
     async with engine.async_session() as session:
         # delete quizparts, that are older than their difficulty or timing
@@ -52,6 +52,15 @@ async def run_job(k: bool, videomaker: VideoMakerBase) -> None:
                 select(QuizPart.id)
                 .join(QuizPart.difficulty)
                 .join(QuizPart.timing)
+                .join(QItemSourceTiming.qitem_source)
+                .join(QItemSource.qitem)
+                .where(
+                    (QItem.category == Category.Opening)
+                    if args.category == "op"
+                    else ((QItem.category == Category.Ending) if args.category == "ed" else True)
+                )
+                .where(QItemSource.added_by.in_(args.source_strategies))
+                .where(QItem.anime_id.in_(args.anime_ids) if len(args.anime_ids) > 0 else True)
                 .where(
                     (QuizPart.updated_at < QItemDifficulty.updated_at)
                     | (QuizPart.updated_at < QItemSourceTiming.updated_at)
@@ -71,16 +80,16 @@ async def run_job(k: bool, videomaker: VideoMakerBase) -> None:
                 label(
                     "d_prio",
                     case(
-                        *[(QItemDifficulty.added_by == sname, i) for i, sname in enumerate(d_strategies)],
-                        else_=len(d_strategies),
+                        *[(QItemDifficulty.added_by == sname, i) for i, sname in enumerate(args.difficulty_strategies)],
+                        else_=len(args.difficulty_strategies),
                     ),
                 ),
                 label("t_id", QItemSourceTiming.id),
                 label(
                     "t_prio",
                     case(
-                        *[(QItemSourceTiming.added_by == sname, i) for i, sname in enumerate(t_strategies)],
-                        else_=len(t_strategies),
+                        *[(QItemSourceTiming.added_by == sname, i) for i, sname in enumerate(args.timing_strategies)],
+                        else_=len(args.timing_strategies),
                     ),
                 ),
                 label("q_id", QItemSource.qitem_id),
@@ -88,7 +97,17 @@ async def run_job(k: bool, videomaker: VideoMakerBase) -> None:
             .distinct(QItemSource.id)
             .join(QItemDifficulty, QItemDifficulty.qitem_id == QItemSource.qitem_id)
             .join(QItemSource.timings)
-            .where(QItemSource.local_fp.isnot(None))
+            .join(QItemSource.qitem)
+            .where(
+                (QItem.category == Category.Opening)
+                if args.category == "op"
+                else ((QItem.category == Category.Ending) if args.category == "ed" else True)
+            )  # only accepted qitem category
+            .where(QItemSourceTiming.added_by.in_(args.timing_strategies))  # only accepted difficulties
+            .where(QItemDifficulty.added_by.in_(args.difficulty_strategies))  # only accepted timings
+            .where(QItemSource.added_by.in_(args.source_strategies))  # only accepted sources
+            .where(QItem.anime_id.in_(args.anime_ids) if len(args.anime_ids) > 0 else True)  # only accepted anime ids
+            .where(QItemSource.local_fp.isnot(None))  # only downloaded
             .order_by(
                 QItemSource.id,
                 literal_column("d_prio"),
@@ -111,60 +130,76 @@ async def run_job(k: bool, videomaker: VideoMakerBase) -> None:
                     QuizPart,
                     (QuizPart.difficulty_id == best_dt_pairs.c.d_id)
                     & (QuizPart.timing_id == best_dt_pairs.c.t_id)
-                    & (QuizPart.style == "classic"),
+                    & (QuizPart.style == args.style),
                 )
                 .where(QuizPart.id.is_(None))
             )
-        ).fetchone()
+        ).all()
 
-        if result is not None:
-            s_id, d_id, t_id, q_id = result
+        if len(result) == 0:
+            await asyncio.sleep(args.wait)
+            return
+
+        videomaker = next(filter(lambda vm: vm.name == args.style, styles))
+        for s_id, d_id, t_id, q_id in result:
             logger.info(
-                f"Running style '{videomaker.name}' on qitem_id={q_id}, "
+                f"Running style '{args.style}' on qitem_id={q_id}, "
                 f"source_id={s_id}, difficulty_id={d_id}, timing_id={t_id}"
             )
             await run_videomaker(t_id, d_id, videomaker)
-            if not k:
-                quizparts_to_delete = (
-                    await session.scalars(
-                        select(QuizPart.id)
-                        .join(QuizPart.timing)
-                        .join(QItemSourceTiming.qitem_source)
-                        .where(QItemSource.id == s_id)
-                        .where(
-                            (QuizPart.difficulty_id != d_id)
-                            | (QuizPart.timing_id != t_id)
-                            | (QuizPart.style != videomaker.name)
-                        )
-                    )
-                ).all()
-
-                if len(quizparts_to_delete) > 0:
-                    logger.info(f"Clearing quizparts with same source_id={s_id} by ids={quizparts_to_delete}")
-                    await session.execute(delete(QuizPart).where(QuizPart.id.in_(quizparts_to_delete)))
-                    await session.commit()
-            return
 
 
-async def main(interval: float, k: bool, videomaker: VideoMakerBase) -> None:
-    rate_limited_run_job = restrict_callrate(interval)(run_job)
+async def main(args: argparse.Namespace) -> None:
     while True:
-        await rate_limited_run_job(k, videomaker)
+        await run_jobs(args)
 
 
 if __name__ == "__main__":
     worker_log_config(str((worker_dir / ".log").resolve()))
     parser = argparse.ArgumentParser()
-    parser.add_argument("style", type=str, help="style of videomaker to use")
-    parser.add_argument("-t", type=float, default=10, help="interval in seconds between job starts")
+    parser.add_argument("style", type=str, choices=[vm.name for vm in styles], help="style of videomaker to use")
+    parser.add_argument("-w", "--wait", type=float, default=10, help="waiting time, if no jobs were found")
     parser.add_argument(
-        "-k",
-        action="store_true",
-        help="keep existing quiz parts, even if better one was created (same source but better difficulty or timing)",
+        "-t",
+        "--timing-strategies",
+        nargs="+",
+        type=str,
+        choices=t_strategies,
+        default=t_strategies,
+        help="timing strategies to accept (in order of priority)",
+    )
+    parser.add_argument(
+        "-d",
+        "--difficulty-strategies",
+        nargs="+",
+        type=str,
+        choices=d_strategies,
+        default=d_strategies,
+        help="difficulty strategies to accept (in order of priority)",
+    )
+    parser.add_argument(
+        "-s",
+        "--source-strategies",
+        nargs="+",
+        type=str,
+        default=s_strategies,
+        choices=s_strategies,
+        help="source finding strategies to accept",
+    )
+    parser.add_argument(
+        "-a",
+        "--anime-ids",
+        nargs="+",
+        type=int,
+        default=[],
+        help="anime ids, to create quizparts for (if not specified, all animes are accepted)",
+    )
+    parser.add_argument(
+        "-c",
+        "--category",
+        type=str,
+        choices=["op", "ed"],
+        help="if specified, create quizparts only for qitems of these categories",
     )
     args = parser.parse_args()
-    try:
-        videomaker = next(filter(lambda vm: vm.name == args.style, styles))
-    except StopIteration:
-        raise ValueError(f"No such style: {args.style}. Possible styles: {[vm.name for vm in styles]}")
-    asyncio.run(main(args.t, args.k, videomaker))
+    asyncio.run(main(args))
