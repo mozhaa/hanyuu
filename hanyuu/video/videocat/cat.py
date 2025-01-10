835f5709f1a93de0aa292b59ff5f78e32d01a702
mozhaa <mozhay2005@gmail.com>
2025-01-10 20:53:20 +0300

    bug fix: resolve path in videocat

--- hanyuu/video/videocat/cat.py
+++ hanyuu/video/videocat/cat.py
@@ -1,19 +1,17 @@
 import ffmpeg
-from typing import List
+from typing import Iterable
 from tempfile import NamedTemporaryFile
 from pathlib import Path
 
 
-def cat(videos: List[str], output_fp: str) -> None:
+def cat(videos: Iterable[str], output_fp: str) -> None:
     tf = NamedTemporaryFile("w+", suffix=".txt", delete_on_close=False)
-    tf.writelines([f"file '{fp}'\n" for fp in videos])
+    tf.writelines([f"file '{Path(fp).resolve()}'\n" for fp in videos])
     tf.close()
     tf_path = tf.name
     try:
         command = ffmpeg.output(ffmpeg.input(tf_path, f="concat", safe=0), output_fp, c="copy")
         print(command.compile())
         command.run()
-    except Exception as e:
+    finally:
         Path(tf_path).unlink()
-        raise e
-    Path(tf_path).unlink()
