ff79f561a4a8afb1d9fca87ca3d3abc97e31318e
mozhaa <mozhay2005@gmail.com>
2026-01-07 15:23:29 +0300

    feat: add strategy_dumb for difficulty

--- src/hanyuu/workers/difficulty/strategies/__init__.py
+++ src/hanyuu/workers/difficulty/strategies/__init__.py
@@ -1,8 +1,9 @@
 from typing import List
 
 from .base import DifficultyStrategy
+from .dumb import Dumb
 from .random import Random
 
-strategies: List[DifficultyStrategy] = [Random("strategy_random")]
+strategies: List[DifficultyStrategy] = [Dumb("strategy_dumb"), Random("strategy_random")]
 
-__all__ = ["DifficultyStrategy", "Random", "strategies"]
+__all__ = ["DifficultyStrategy", "Dumb", "Random", "strategies"]
---
+++ src/hanyuu/workers/difficulty/strategies/dumb.py
@@ -0,0 +1,30 @@
+import math
+
+from hanyuu.database.main.connection import get_engine
+from hanyuu.database.main.models import Anime, QItem, QItemDifficulty
+
+from .base import DifficultyStrategy
+
+
+class Dumb(DifficultyStrategy):
+    async def run(self, qitem_id: int) -> None:
+        engine = await get_engine()
+        async with engine.async_session() as session:
+            qitem = await session.get(QItem, qitem_id)
+            if qitem is None:
+                raise RuntimeError(f"no such qitem with {qitem_id=}")
+            anime: Anime = await qitem.awaitable_attrs.anime
+
+            watch_count = anime.shiki_completed + anime.shiki_watching
+            number = qitem.number
+
+            value = 241.75 - 44.54 * math.log10(watch_count) + 2.48 * number
+            value = max(0, min(100, value))
+
+            difficulty = QItemDifficulty(
+                qitem_id=qitem_id,
+                value=value,
+                added_by=self.name,
+            )
+            session.add(difficulty)
+            await session.commit()
